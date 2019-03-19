bd4231fae1896dcaa015a5a91dc5cae7375fe6c9
skuramatata <skuramatata@hotmail.com>
2019-03-19 12:56:54 +0800

    11

--- TestProject/Scripts/app.js
+++ TestProject/Scripts/app.js
@@ -1,16 +1,14 @@
-﻿import Vue from 'vue';
+import Vue from 'vue';
 import App from './app.vue';
 import router from "./router";
-
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
-
 Vue.use(iView, {
     size: 'small'
 });
-
 new Vue({
     el: '#app',
     router: router,
     render: h => h(App)
 });
+//# sourceMappingURL=app.js.map
